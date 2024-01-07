5f1bb27f6883e3958cc6ed87ee4b4942f4511f8d
alex <your-email@example.com>
2024-01-07 17:12:53 +0200

    test

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -11,23 +11,11 @@ using WebApp.Models;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Azure.Core;
-SecretClientOptions options = new SecretClientOptions()
-    {
-        Retry =
-        {
-            Delay= TimeSpan.FromSeconds(2),
-            MaxDelay = TimeSpan.FromSeconds(16),
-            MaxRetries = 5,
-            Mode = RetryMode.Exponential
-         }
-    };
-var client = new SecretClient(new Uri("https://test-kvalex.vault.azure.net/"), new DefaultAzureCredential(),options);
 
-KeyVaultSecret secret = client.GetSecret("test");
-
-
-string secretValue = secret.Value;
-
+var client = new SecretClient(new Uri("https://test-kvalex.vault.azure.net/"), new DefaultAzureCredential());  
+  
+KeyVaultSecret secret = client.GetSecret("test");  
+string secretValue = secret.Value;  
 namespace WebApp
 {
     public class Program
